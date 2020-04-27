e4a3cfe2a11deb63d4b2a5547d031a174e1d43b3
nghiepnq-dgh <nghiepnq@digitechglobalco.com>
2020-04-27 23:21:20 +0700

    [Feature] user get

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const app = express();
 
+const mockUserData = [{name: 'Make'}, {name: 'Yeb'}];
+
+app.get('/users', function(req, res) {
+  res.json({success: true, message: 'Success get user', users: mockUserData});
+});
+
 app.listen(8000, function() {
-	console.log("server is running");
-})
+  console.log('server is running');
+});
