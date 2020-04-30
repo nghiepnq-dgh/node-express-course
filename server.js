03c0284acf330a90f17b77435e36355caa6d3e69
nghiepnq-dgh <nghiepnq@digitechglobalco.com>
2020-04-30 14:28:53 +0700

    [Feature] user get by id

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-const mockUserData = [{name: 'Make'}, {name: 'Yeb'}];
+const mockUserData = [{ name: "Make" }, { name: "Yeb" }];
 
-app.get('/users', function(req, res) {
-  res.json({success: true, message: 'Success get user', users: mockUserData});
+app.get("/users", function(req, res) {
+    res.json({
+        success: true,
+        message: "Success get user",
+        users: mockUserData
+    });
+});
+
+app.get("/users/:id", function(req, res) {
+    console.log(req.param.id);
+    res.json({
+        success: true,
+        message: "get one user",
+        user: req.param.id
+    });
 });
 
 app.listen(8000, function() {
-  console.log('server is running');
+    console.log("server is running");
 });
